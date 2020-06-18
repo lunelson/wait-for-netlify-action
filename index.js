be10b7ad512985c804500daa93266fe637f00fe8
Lu Nelson <lunelson@gmail.com>
2020-06-18 16:07:56 +0200

    Refactor to use Netlify API, to find Deploy ID URL (#1)

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const axios = require("axios");
-
-const waitForUrl = async (url, MAX_TIMEOUT, { headers }) => {
-  const iterations = MAX_TIMEOUT / 2;
-  for (let i = 0; i < iterations; i++) {
-    try {
-      await axios.get(url, { headers });
-      return;
-    } catch (e) {
-      console.log("Url unavailable, retrying...");
-      await new Promise((r) => setTimeout(r, 2000));
-    }
-  }
-  core.setFailed(`Timeout reached: Unable to connect to ${url}`);
-};
+const Netlify = require('netlify');
 
 const run = async () => {
   try {
-    const PR_NUMBER = github.context.payload.number;
-    if (!PR_NUMBER) {
-      core.setFailed(
-        "Action must be run in conjunction with the `pull_request` event"
-      );
+    const accessToken = core.getInput('access_token')
+    const accountSlug = core.getInput('account_slug')
+    const siteName = core.getInput('site_name')
+    const maxTimeout = Number(core.getInput("timeout_secs")) || 60;
+    const commitRef = core.getInput('commit_ref') || github.context.payload.head; // allow this input for testing only
+    /*
+      insert checks here, to fail if any of these inputs are invalid
+    */
+    const client = new Netlify(accessToken)
+    const getDeployId = async () => {
+      let sites, site, deploys, deploy;
+      try {
+        sites = await client.listSitesForAccount({account_slug: accountSlug});
+        site = sites.find(site => site.name === siteName);
+      } catch (e) {
+        const errMsg = 'account slug or site name invalid';
+        core.warning(errMsg);
+        throw new Error(errMsg);
+      }
+      const iterations = maxTimeout / 3;
+      for (let i = 0; i < iterations; i++) {
+        try {
+          deploys = await client.listSiteDeploys({ site_id: site.id });
+          deploy = deploys.find(deploy => deploy.commit_ref === commitRef);
+          return deploy.id;
+        } catch (e) {
+          core.warning('commit deploy not (yet) available; retrying in 3s')
+          await new Promise(r => setTimeout(r, 3000));
+        }
+      }
+      const errMsg = 'timed out: commit deploy was unavailable';
+      core.warning(errMsg);
+      throw new Error(errMsg);
     }
-    const MAX_TIMEOUT = Number(core.getInput("max_timeout")) || 60;
-    const siteName = core.getInput("site_name");
-    if (!siteName) {
-      core.setFailed("Required field `site_name` was not provided");
-    }
-    const url = `https://deploy-preview-${PR_NUMBER}--${siteName}.netlify.app`;
+    core.info(`Waiting to resolve Netlify deploy ID from last commit`);
+    const deployId = await getDeployId();
+    const url = `https://${deployId}--${siteName}.netlify.app`;
     core.setOutput("url", url);
-    const extraHeaders = core.getInput("request_headers");
-    const headers = !extraHeaders ? {} : JSON.parse(extraHeaders)
-    console.log(`Waiting for a 200 from: ${url}`);
-    await waitForUrl(url, MAX_TIMEOUT, {
-      headers,
-    });
   } catch (error) {
     core.setFailed(error.message);
   }
